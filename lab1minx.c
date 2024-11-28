9dd60d8560629cf9178e3b43a31bba79b37dcaac
6704062611034 <s6704062611034@email.kmutnb.ac.th>
2024-11-28 14:46:17 +0700

    commit work2 done

--- lab1minx.c
+++ lab1minx.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 int main() {
-	int num1=5;
-	int num2=10;
+	int num1;
+	int num2;
+	printf("Type a number: \n");
+	scanf("%d", &num1);
+	printf("Type a number: \n");
+	scanf("%d", &num2);
 	if (num1>num2) {
-		printf("num1 greater than num2");
+		printf("num1 %d greater than num2", num1-num2);
 	} else {
-		printf("num2 greater than num1");
+		printf("num2 %d greater than num1", num2-num1);
 	}
 }
