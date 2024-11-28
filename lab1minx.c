a56ddc86fe23eaecea8b41a151d1d09224e9d6f3
6704062611034 <s6704062611034@email.kmutnb.ac.th>
2024-11-28 14:58:23 +0700

    commit work3 เลขข้างกัน

--- lab1minx.c
+++ lab1minx.c
@@ -2,10 +2,8 @@
 int main() {
 	int num1;
 	int num2;
-	printf("Type a number: \n");
-	scanf("%d", &num1);
-	printf("Type a number: \n");
-	scanf("%d", &num2);
+	printf("num1 num2");
+	scanf("%d %d", &num1,&num2);
 	if (num1>num2) {
 		printf("num1 %d greater than num2", num1-num2);
 	} else {
